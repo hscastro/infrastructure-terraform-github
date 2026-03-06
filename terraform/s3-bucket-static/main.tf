be567b044d8c090e2953693fce897887787f6ac5
hacastro <halisonsc5@gmail.com>
2026-03-06 17:41:34 -0300

    Revisão do main.tf

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,54 +1,56 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
 variable "bucket_name" {
-    type = string
+  type = string
 }
 
+resource "aws_s3_bucket" "static_site_bucket" {
+  bucket = "static-site-${var.bucket_name}"
 
-resource  "aws_s3_bucket" "static_site_bucket" {
-    bucket = "static-site-${var.bucket_name}"
-
-    website {
-        index_document = "index.html"
-        error_document = "400.html"
-    } 
-
-    tags {
-        Name = "Static Site Bucket"
-        Environment = "Production"
-    }
+  tags = {
+    Name        = "Static Site Bucket"
+    Environment = "Production"
+  }
 }
 
+resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "400.html"
+  }
+}
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.id
 
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_bucket = false
- }
-
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.id
 
-    rule {
-        object_ownership = "BucketOwnershipPreferred"
-    }
- }
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
+resource "aws_s3_bucket_acl" "static_site_bucket" {
 
-resource "aws_s3_bucket_acls" "static_site_bucket" {
-    
-    depends_on = [
-        aws_s3_bucket_public_access_block.static_site_bucket,
-        aws_s3_bucket_ownership_controls.static_site_bucket,        
-    ]
-    
-    bucket = aws_s3_bucket.static_site_bucket.id
-    acls = "public-read"
-    
- }
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site_bucket,
+    aws_s3_bucket_ownership_controls.static_site_bucket
+  ]
+
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
+}
