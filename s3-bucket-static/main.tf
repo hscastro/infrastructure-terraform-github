a056648a709137b4c2dbf8298f7442e506c88577
hacastro <halisonsc5@gmail.com>
2026-03-06 15:54:23 -0300

    first commit

---
+++ s3-bucket-static/main.tf
@@ -0,0 +1,54 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+variables "bucket_name" {
+    type = String
+}
+
+
+resource  "aws_s3_bucket" "static_site_bucket" {
+    bucket = "static-site-${var.bucket_name}"
+
+    website {
+        index_document = "index.html"
+        error_document = "400.html"
+    } 
+
+    tags {
+        Name = "Static Site Bucket"
+        Environment = "Production"
+    }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_bucket = false
+ }
+
+
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+    rule {
+        object_ownership = "BucketOwnershipPreferred"
+    }
+ }
+
+
+resource "aws_s3_bucket_acls" "static_site_bucket" {
+    
+    depends_on = [
+        aws_s3_bucket_public_access_block.static_site_bucket,
+        aws_s3_bucket_ownership_controls.static_site_bucket,        
+    ]
+    
+    bucket = aws_s3_bucket.static_site_bucket.id
+    acls = "public-read"
+    
+ }
